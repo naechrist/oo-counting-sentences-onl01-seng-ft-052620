1f0b8b9eb90990762cc38004aa5cb030b98a3f4a
Jenae Christensen <729jenae.christensen@gmail.com>
2020-06-05 16:19:22 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,6 +15,7 @@ class String
   end
 
   def count_sentences
-    self.gsub(".", "142").gsub(".", "142").gsub(".", "142").split("142")
+    sentences = self.gsub(".", "142").gsub(".", "142").gsub(".", "142").split("142").reject {|sentence| sentence == ""}
+    sentences.count
   end
 end
